087a5ad48fd0880fc52585f91f0d16d79dbe241a
H Hatfield <hhatfield@deseretdigital.com>
2014-07-15 14:51:38 -0600

    adding local config

---
+++ config/autoload/local.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'db' => array(
+        'adapters' => array(
+            'contact' => array(
+                'driver' => 'Pdo_Mysql',
+                'database' => 'contact',
+                'username' => 'root',
+                'password' => 'root',
+            ),
+        ),
+    ),
+);
